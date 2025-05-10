eb7e8034692d94de1372fa7310450e508817b2e9
Juraj Zikmund <jur.zikmund@gmail.com>
2025-05-10 11:41:29 +0200

    TWEAK: Major changes
    project.toml improvements, cibuildwheel.
    gitignore and gitattributes are managing the build artifacts.
    Automatic version generation based on git tag.

--- src/vanillapdf/__init__.py
+++ src/vanillapdf/__init__.py
@@ -1,3 +1,8 @@
 from .document import Document
 
-__all__ = ["Document"]
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0.dev"
+
+__all__ = ["Document", "__version__"]
